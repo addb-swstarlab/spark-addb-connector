5dc3a432ec6f2af0d3b61275695968484dc1850c
Won Gi Choi <cwk1412@yonsei.ac.kr>
2018-06-01 14:20:54 +0900

    [FEATURE] Transform In to set of EqualTo
    Former-commit-id: 01683d4a71b202d55e94c82a22a64da08244b41f

--- src/main/scala/kr/ac/yonsei/delab/addb_srconnector/util/KeyUtil.scala
+++ src/main/scala/kr/ac/yonsei/delab/addb_srconnector/util/KeyUtil.scala
@@ -133,70 +133,81 @@ object FilterUtil {
     def makeFilterString(f: Filter, stack: Stack[String]) : Unit = {
     f match {
       case Or(_,_) =>  { 
-        stack.push("Or:")
+        stack.push(":Or")
         makeFilterString(f.asInstanceOf[Or].left, stack)
         makeFilterString(f.asInstanceOf[Or].right, stack)
       }
       case And(_,_) =>  {
-        stack.push("And:")
+        stack.push(":And")
         makeFilterString(f.asInstanceOf[And].left, stack)
         makeFilterString(f.asInstanceOf[And].right, stack)
       }
       
       case EqualTo(_,_) => {
-        stack.push("EqaulTo:")
-        stack.push(f.asInstanceOf[EqualTo].attribute)
-        stack.push(f.asInstanceOf[EqualTo].value.toString())
+        stack.push(":EqaulTo")
+        stack.push("*" + f.asInstanceOf[EqualTo].attribute)
+        stack.push("*" + f.asInstanceOf[EqualTo].value.toString())
       }
       
       case GreaterThan(_, _) => { 
-        stack.push("GreaterThan:")
-        stack.push(f.asInstanceOf[GreaterThan].attribute)
-        stack.push(f.asInstanceOf[GreaterThan].value.toString())
+        stack.push(":GreaterThan")
+        stack.push("*" + f.asInstanceOf[GreaterThan].attribute)
+        stack.push("*" + f.asInstanceOf[GreaterThan].value.toString())
       }
       
       case GreaterThanOrEqual(_, _) => {
-        stack.push("GreaterThanOrEqual:")
-        stack.push(f.asInstanceOf[GreaterThanOrEqual].attribute)
-        stack.push(f.asInstanceOf[GreaterThanOrEqual].value.toString())
+        stack.push(":GreaterThanOrEqual")
+        stack.push("*" + f.asInstanceOf[GreaterThanOrEqual].attribute)
+        stack.push("*" + f.asInstanceOf[GreaterThanOrEqual].value.toString())
       }
       case LessThan(_, _) => { 
-        stack.push("LessThan:")
-        stack.push(f.asInstanceOf[LessThan].attribute)
-        stack.push(f.asInstanceOf[LessThan].value.toString())
+        stack.push(":LessThan")
+        stack.push("*" + f.asInstanceOf[LessThan].attribute)
+        stack.push("*" + f.asInstanceOf[LessThan].value.toString())
       }
       case LessThanOrEqual(_, _) => {
-        stack.push("LessThanOrEqual:")
-        stack.push(f.asInstanceOf[LessThanOrEqual].attribute)
-        stack.push(f.asInstanceOf[LessThanOrEqual].value.toString())
+        stack.push(":LessThanOrEqual")
+        stack.push("*" + f.asInstanceOf[LessThanOrEqual].attribute)
+        stack.push("*" + f.asInstanceOf[LessThanOrEqual].value.toString())
       }
       case In(_, _) => {
-        stack.push("In:")
-        stack.push(f.asInstanceOf[In].attribute)
-        stack.push(f.asInstanceOf[In].values.toString())
+        /** Transform set of EqualTo **/
+        //stack.push("In:")
+        var i = 0        
+        val col = f.asInstanceOf[In].attribute
+        val arrLen = f.asInstanceOf[In].values.length
+        for (i <- 0 until arrLen - 1) {
+          stack.push(":Or")
+        }
+        f.asInstanceOf[In].values.foreach{
+          x =>
+          stack.push(":EqualTo")
+          stack.push("*" + col)
+          stack.push("*" + x.toString())
+        }
       }
       case IsNull(_)=> {
-        stack.push("IsNull:")
-        stack.push(f.asInstanceOf[IsNull].attribute)
+        stack.push(":IsNull")
+        stack.push("*" + f.asInstanceOf[IsNull].attribute)
        }
       case IsNotNull(_) => {
-        stack.push("IsNotNull:")
-        stack.push(f.asInstanceOf[IsNotNull].attribute)
+        stack.push(":IsNotNull")
+        stack.push("*" + f.asInstanceOf[IsNotNull].attribute)
        }
       case StringStartsWith(_, _) => {
-        stack.push("StringStartsWith:")
-        stack.push(f.asInstanceOf[StringStartsWith].attribute)
-        stack.push(f.asInstanceOf[StringStartsWith].value)
+        stack.push(":StringStartsWith")
+        stack.push("*" + f.asInstanceOf[StringStartsWith].attribute)
+        stack.push("*" + f.asInstanceOf[StringStartsWith].value)
       }
       case StringEndsWith(_, _) => {
-        stack.push("StringEndsWith:")
-        stack.push(f.asInstanceOf[StringEndsWith].attribute)
-        stack.push(f.asInstanceOf[StringEndsWith].value)
+        stack.push(":StringEndsWith")
+        stack.push("*" + f.asInstanceOf[StringEndsWith].attribute)
+        stack.push("*" + f.asInstanceOf[StringEndsWith].value)
       }
       case StringContains(_, _) => {
-        stack.push("StringContains:")
-        stack.push(f.asInstanceOf[StringContains].attribute)
-        stack.push(f.asInstanceOf[StringContains].value)
+        stack.push(":StringContains")
+        stack.push("*" + f.asInstanceOf[StringContains].attribute)
+        stack.push("*" + f.asInstanceOf[StringContains].value)
       }
     }
   }
